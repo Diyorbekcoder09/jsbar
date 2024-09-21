40fff5cdabfa4fa732865bffd54407b99df21bb1
diyorbek2009 <diyorbekcoder09@gmail.com>
2024-09-21 15:29:14 +0500

    hover js

---
+++ assets/Js/app.js
@@ -0,0 +1,21 @@
+const btnEl = document.querySelector(".fa-bars")
+const boxEl = document.querySelector(".box")
+
+btnEl.addEventListener("click", () => {
+  boxEl.style.marginLeft = "0px";
+  fabars.style.display = "none"
+});
+
+
+const btnEl1 = document.querySelector(".fa-x")
+const boxEl1 = document.querySelector(".box")
+
+btnEl1.addEventListener("click", () => {
+  boxEl1.style.marginLeft = "-135px";
+  fabars.style.display = "block"
+
+});
+
+
+
+
